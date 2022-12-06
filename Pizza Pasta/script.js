2fdc9e5b30666463fbc427871e37070b26b2987f
JP <jpcodingjourney@JPs-MacBook-Air.local>
2022-12-06 15:32:05 +1100

    Pizza Pasta

---
+++ Pizza Pasta/script.js
@@ -0,0 +1,8 @@
+let whatFoodYouLike = prompt('what you like')
+if (whatFoodYouLike === "pizza") {
+    alert("yay pizza!!")
+} else if (whatFoodYouLike === "pasta") {
+    alert("ooh I love pasta!!")
+} else {
+    alert("brussel sprouts for you!")
+}
