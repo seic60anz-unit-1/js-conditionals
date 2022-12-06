87cf0656407f69a97c6b9706285ad3fade2e1562
JP <jpcodingjourney@JPs-MacBook-Air.local>
2022-12-06 16:32:41 +1100

    Valentine's Day Flowchart

---
+++ Valentine's Day Flowchart/script.js
@@ -0,0 +1,12 @@
+let inRelationship = false
+let relationshipIsSerious = false
+
+if (inRelationship === true) {
+    if (relationshipIsSerious === true) {
+        console.log('Romantic dinner for two')
+    } else {
+        console.log('Head to the closest bar')
+    } 
+} else {
+    console.log('Head to the closest bar')
+}
