52c9cbac3e0151341537e045fda84d5f10bea3e2
Penelope Chen <pen.c828@gmail.com>
2022-12-06 16:10:14 +1100

    JS-Conditionals complete

--- script.js
+++ script.js
@@ -60,3 +60,19 @@ if (inRelo === true) {
 }
 
 // Letter to Santa Flowchart
+const nice = true 
+const haveStamp = true
+if (nice === true) {
+    console.log("Write your wishlist and letter to Santa.")
+    console.log("Address your envelope with '1 Candy Cane Lane, North Pole'.")
+    console.log("Fold and place your letter in your envelope.")
+    if (haveStamp === true) {
+        console.log("Place it on the envelope.")
+    } else {
+        console.log("Get a stamp from teacher.")
+        console.log("Place it on the envelope.")
+        console.log("You're done! Let's mail your letter to Santa!")
+    }
+} else { 
+    console.log("Do a good deed!")
+}
