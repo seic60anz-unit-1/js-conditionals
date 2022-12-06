2d4323709fbbee8371303543f36f0f15fcaa98f5
Penelope Chen <pen.c828@gmail.com>
2022-12-06 15:05:01 +1100

    2 exercises done

---
+++ script.js
@@ -0,0 +1,23 @@
+// Exam Marks
+let studentScore = 80
+if (studentScore >= 80) {
+    console.log("You got an A!")
+} else if (studentScore >= 70) {
+    console.log("You got a B!")
+} else if (studentScore >= 60) {
+    console.log("You got a C!")
+} else if (studentScore >= 50) {
+    console.log("You got a D!")
+} else {
+    console.log("You need to retake the exam.")
+}
+
+// Pizza/Pasta 
+const foodPreference = prompt("What do you want to eat?")
+if (foodPreference = pizza) {
+    console.log("yay pizza!!")
+} else if (foodPreference = pasta) {
+    console.log("ooh I love pasta!!")
+} else {
+    console.log("brussel sprouts for you!")
+}
