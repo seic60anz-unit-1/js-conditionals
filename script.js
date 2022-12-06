bb87f778106cf5189b0447757a6388976364986a
Wing Fung Lau <wingfunglau@Wings-MacBook-Pro.local>
2022-12-06 15:16:04 +1100

    Pizza/Pasta

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Exam Mark
-let studentScore = prompt()
+let studentScore = prompt("What is your score?")
 if (studentScore >= 80) {
     console.log('You\'ve got an A.')
 } else if (studentScore >= 70) {
@@ -10,4 +10,17 @@ if (studentScore >= 80) {
     console.log('You\'ve got a D.')
 } else {
     console.log('You have to re-take.')
+}
+
+//Pizza/Pasta
+let pizza = prompt("Do you like pizza?","Y/N")
+if (pizza == "Y" || pizza == "y") {
+    console.log("yay pizza!")
+} else {
+    let pasta = prompt("Do you like pasta?","Y/N")
+    if (pasta == "Y" || pasta == "y") {
+        console.log("yay pasta!")
+    } else {
+        console.log("brussel sprouts for you!")
+    }
 }
