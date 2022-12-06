ea0d569da210f3eb8853246244e4c54842e1b1c7
Wayne Kenny <waynekenny@Waynes-MacBook-Air.local>
2022-12-06 16:34:13 +1100

    Script test

---
+++ script.js
@@ -0,0 +1,96 @@
+
+// Exam Marks
+// const studentScore = (35)
+// if (studentScore > 80) {
+//     console.log('You got a A')
+// } else if (studentScore > 70) {
+//     console.log('You got a B')
+// } else if (studentScore > 60) {
+//     console.log('You got a C') 
+// } else if (studentScore > 50) {
+//     console.log('You got a D')
+// } else {
+//     console.log('re-take the exam')
+// }
+
+
+//Pizza/Pasta
+// const foodChoice = prompt('What would you like to eat')
+// if (foodChoice.toLowerCase() === 'pizza') {
+//     console.log('Yay Pizza!!')
+// } else if (foodChoice.toLocaleLowerCase() === 'pasta') {
+//     console.log('ooh I love pasta!!')
+// } else {
+//     console.log('brussel sprouts for you')
+// }
+
+
+// Bouncer 1
+// const age = (76)
+// if (age > 75) {
+//     console.log('Are you sure you want to be here?')
+// } else if(age >= 21) {
+//     console.log('Come on in!')
+// } else if (age >= 18) {
+//     console.log('Come on in, (but no drinking)!')
+// } else {
+//     console.log("You're too young to be in here!")
+//}
+
+
+// Bouncer 2
+// const hasID = true
+// const age = 75
+// if (hasID) {
+//     if (age >= 75) {
+//         console.log('Are you sure you want to be here?')
+//     } else if (age >= 21) {
+//         console.log('Come on in!')
+//     } else if (age >= 18) {
+//         console.log('Come on in, (but no drinking)!')
+//     } else
+//         console.log("You're too young to be in here!")
+// } else {
+//     console.log('no ID, no entry')
+// }
+
+
+// Valentines Day Flowchart
+// const hasRelationship = true
+// const isSerious = false
+// if (hasRelationship) {
+//     if (isSerious) {
+//         console.log('Romantic dinner for two')
+//     } else {
+//         console.log('Lets head to the bar')
+//     }
+// } else {
+//     console.log('Lets head to the bar')
+// }   
+
+
+// Letter to Santa Flowchart
+
+const beenNice = false
+const hasStamp = false
+
+if (beenNice) {
+    console.log('Write your wishlist and letter to Santa')
+    console.log('Address your envelope with "1 Candy Cane Lane, North Pole"')
+    console.log('Fold and place your letter in your envelope')
+    if (hasStamp) {
+        console.log('Place it on the envelope')
+        console.log("You're done! Let's mail your letter to Santa!")
+    } else {
+        console.log('get a stamp from the Teacher')
+        console.log('Place it on the envelope')
+        console.log("You're done! Let's mail your letter to Santa!")
+    }
+} else {
+    console.log('Do a good deed')
+}
+
+
+
+
+
