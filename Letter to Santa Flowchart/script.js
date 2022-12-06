30b77df0f36aa66e83040ff8f064367abe321b98
JP <jpcodingjourney@JPs-MacBook-Air.local>
2022-12-06 16:47:04 +1100

    Letter to Santa Flowchart

---
+++ Letter to Santa Flowchart/script.js
@@ -0,0 +1,18 @@
+let didYouBehave = false
+let haveStamp = false
+
+if (didYouBehave === true) {
+    console.log('Write your wishlist and letter to Santa')
+    console.log('Address your envelope with "1 Candy Cane Lane, North Pole".') 
+    console.log('Fold and place your letter in your envelope')
+    if (haveStamp === true) {
+        console.log('Place it on the envelope')
+        console.log('You\'re done! Let\'s mail your letter to santa')
+    } else {
+        console.log('get a stamp from teacher')
+        console.log('Place it on the envelope')
+        console.log('You\'re done! Let\'s mail your letter to santa');
+    }
+} else {
+    console.log('Do a good deed')
+}
