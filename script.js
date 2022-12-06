9574d9b0bd9558346dc525885a1182c3a23267b8
Wing Fung Lau <wingfunglau@Wings-MacBook-Pro.local>
2022-12-06 15:03:25 +1100

    Exam Mark

---
+++ script.js
@@ -0,0 +1,13 @@
+//Exam Mark
+let studentScore = prompt()
+if (studentScore >= 80) {
+    console.log('You\'ve got an A.')
+} else if (studentScore >= 70) {
+    console.log('You\'ve got a B.')
+} else if (studentScore >= 60) {
+    console.log('You\'ve got a C.')
+} else if (studentScore >= 50) {
+    console.log('You\'ve got a D.')
+} else {
+    console.log('You have to re-take.')
+}
